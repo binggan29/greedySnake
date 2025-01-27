8569e39db725966d262012a5bf3c7c7b1311861d
binggan29 <userxhm@outlook.com>
2025-01-27 13:48:51 +0800

    change food_x to food_X

--- greedySnake.c
+++ greedySnake.c
@@ -6,7 +6,7 @@
 #define Width 30
 int canvas[High][Width]={0};
 void moveSnakeByDirection();
-int  food_x,food_y;
+int  food_X,food_y;
 int goods_x,goods_y;
 int life;
 void updateWithoutlnput();
@@ -84,9 +84,9 @@ void startup()
     {
         canvas[High/2][Width/2-i]=i+1;
     }
-    food_x=rand()%(High-5)+2;
+    food_X=rand()%(High-5)+2;
     food_y=rand()%(Width-5)+2;
-    canvas[food_x][food_y]=-2;
+    canvas[food_X][food_y]=-2;
     goods_x=rand()%(High-5)+3;
     goods_y=rand()%(Width-5)+3;
     canvas[goods_x][goods_y]=-3;
@@ -173,12 +173,12 @@ void moveSnakeByDirection(int moveDirection)
             }
     }
     canvas[newHead_i][newHead_j]=1;
-    if (newHead_i==food_x&&newHead_j==food_y)
+    if (newHead_i==food_X&&newHead_j==food_y)
     {
         
-        food_x=rand()%(High-5)+2;
+        food_X=rand()%(High-5)+2;
         food_y=rand()%(Width-5)+2;
-        canvas[food_x][food_y]=-2;
+        canvas[food_X][food_y]=-2;
     }else if(newHead_i==goods_x&&newHead_j==goods_y){
         life++;
         printf("life=%d",life);
